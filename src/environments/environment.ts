eb5f7bc647ce48b6a427b733925a777a516ef294
auxji <andreajovanovic00@example.com>
2025-10-16 22:06:44 +0200

    api

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  timeEntriesUrl: 'https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries',
+  timeEntriesKey: 'vO17RnE8vuzXzPJo5eaLLjXjmRW07law99QTD90zat9FfOQJKKUcgQ==' // key
+};
